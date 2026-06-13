6faa0dd26e4ef912d339e7cfd92b0cbee90ec285
mas827 <98495693+mas827@users.noreply.github.com>
2026-06-13 21:20:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+// Service Worker - オフライン対応
+const CACHE = 'fe-exam-v2';
+const ASSETS = [
+  './', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png',
+  './katex/katex.min.css',
+  './katex/fonts/KaTeX_AMS-Regular.woff2',
+  './katex/fonts/KaTeX_Caligraphic-Bold.woff2',
+  './katex/fonts/KaTeX_Caligraphic-Regular.woff2',
+  './katex/fonts/KaTeX_Fraktur-Bold.woff2',
+  './katex/fonts/KaTeX_Fraktur-Regular.woff2',
+  './katex/fonts/KaTeX_Main-Bold.woff2',
+  './katex/fonts/KaTeX_Main-BoldItalic.woff2',
+  './katex/fonts/KaTeX_Main-Italic.woff2',
+  './katex/fonts/KaTeX_Main-Regular.woff2',
+  './katex/fonts/KaTeX_Math-BoldItalic.woff2',
+  './katex/fonts/KaTeX_Math-Italic.woff2',
+  './katex/fonts/KaTeX_SansSerif-Bold.woff2',
+  './katex/fonts/KaTeX_SansSerif-Italic.woff2',
+  './katex/fonts/KaTeX_SansSerif-Regular.woff2',
+  './katex/fonts/KaTeX_Script-Regular.woff2',
+  './katex/fonts/KaTeX_Size1-Regular.woff2',
+  './katex/fonts/KaTeX_Size2-Regular.woff2',
+  './katex/fonts/KaTeX_Size3-Regular.woff2',
+  './katex/fonts/KaTeX_Size4-Regular.woff2',
+  './katex/fonts/KaTeX_Typewriter-Regular.woff2',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((res) => {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res;
+        const clone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+        return res;
+      });
+    })
+  );
+});
