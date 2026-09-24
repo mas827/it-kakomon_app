59e9bb809d2d167876be1cb48618af74d6d8c0f9
mas827 <98495693+mas827@users.noreply.github.com>
2026-09-25 00:47:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - オフライン対応
-const CACHE = 'fe-exam-v25';
+const CACHE = 'fe-exam-v26';
 const ASSETS = [
   './', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png',
   './katex/katex.min.css',
